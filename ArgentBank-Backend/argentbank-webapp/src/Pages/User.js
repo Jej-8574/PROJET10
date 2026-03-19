b28baca445e1253c601fbb37bb19555c12c4e249
Jérémy Catoire <j.catoire60@gmail.com>
2026-03-19 17:17:10 +0100

    page user fini

---
+++ ArgentBank-Backend/argentbank-webapp/src/Pages/User.js
@@ -0,0 +1,16 @@
+import './main.css';
+import Footer from '../components/footer';
+import Navbar from '../components/navbar';
+
+function User() {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <User />
+      </main>
+      <Footer />
+    </>
+  );
+}
+export default User;
