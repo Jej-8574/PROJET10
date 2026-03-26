814376cafc9f05df6f336e50aee1a7158a2d2e8b
Jérémy Catoire <j.catoire60@gmail.com>
2026-03-26 16:59:21 +0100

    bien avancé

--- ArgentBank-Frontend-main/REACT/src/pages/SignIn.jsx
+++ ArgentBank-Frontend-main/REACT/src/pages/SignIn.jsx
@@ -1,5 +1,38 @@
-function Signin() {
-  return <h1>Sign In</h1>
+import Header from '../components/Header'
+import Footer from '../components/footer'
+
+function SignIn() {
+  return (
+    <>
+      <Header />
+      <main className="main bg-dark">
+        <section className="sign-in-content">
+          <h1>Sign In</h1>
+          <form>
+            <div className="input-wrapper">
+              <label htmlFor="username">Username</label>
+              <input type="text" id="username" />
+            </div>
+
+            <div className="input-wrapper">
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" />
+            </div>
+
+            <div className="input-remember">
+              <input type="checkbox" id="remember-me" />
+              <label htmlFor="remember-me">Remember me</label>
+            </div>
+
+            <button type="submit" className="sign-in-button">
+              Sign In
+            </button>
+          </form>
+        </section>
+      </main>
+      <Footer />
+    </>
+  )
 }
 
-export default Signin
+export default SignIn
